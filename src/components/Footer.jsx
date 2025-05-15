f7384e266307328a7ad00a48c9cc5de44da677ec
melok2207 <132545373+melok2207@users.noreply.github.com>
2025-05-15 18:28:55 +0300

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer className="bg-gray-200 text-center py-4">
+      <p>Зв'язок: <a href="https://github.com/yourusername">GitHub</a></p>
+    </footer>
+  );
+}
